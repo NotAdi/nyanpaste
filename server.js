75dc31190ea9028494931b6b870265357b6e904e
notadi <adityam0338@gmail.com>
2023-07-23 04:01:37 +0530

    Custom URLS: Added the ability to add custom urls

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ app.use(cors());
 connect();
 
 
-
-
 app.get('/', function(req, res){
     // res.send('hi');
     const code= `Welcome to nyanpaste!
@@ -54,33 +52,40 @@ app.post('/save', async function(req, res){
         // console.log(result);
         const id= result.insertedId.toString();
         // console.log(id);
-        // if(customNameInput)
-        res.redirect(`/${id}`);
+        if(customNameInput){
+            res.redirect(`/${customNameInput}`);
+        }
+        else
+            res.redirect(`/${id}`);
 
     } catch(e){
         console.log(e);
         res.render("newPage", {value});
     }
-    // console.log(value);
 });
 
 app.get(`/:id`, async function(req, res){
     // const id= req.params.id;
     try{
-        // console.log(id);
-        // const id= '64bbc8ee965438224c2220f4';
+
         const db= client.db('nyanpaste');
         const pastesCollection= db.collection('pastes');
         let id= req.params.id;
-        console.log(req.params);
+
         console.log(id);
-        // const nid = ObjectId(id)
-        const paste= await pastesCollection.findOne( {_id: new ObjectId(id)} );
-        // console.log("Reached here");
+        let paste="";
+        console.log(id.length);
+
+        if(id.length===24)
+            paste= await pastesCollection.findOne( {_id: new ObjectId(id)} );
+        else
+            paste= await pastesCollection.findOne( {'customNameInput': id} );
+
+        console.log(paste);
         if(!paste){
             console.log("not found");
         }
-        // console.log(paste.value);
+
         res.render("codePage", {
             code: paste.value,
             lineNo: paste.value.split('\n').length
